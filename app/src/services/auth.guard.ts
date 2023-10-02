480a8671f89e84ed2b310d25b7fa7de05e64ddfb
AdachiGayming <benjaminspeyerlarsen@gmail.com>
2023-10-02 16:02:38 +0200

    Fixed routing issue in Auth.Guard

--- app/src/services/auth.guard.ts
+++ app/src/services/auth.guard.ts
@@ -2,20 +2,20 @@ import React from "react";
 import { useNavigate } from "react-router-dom";
 
 // checking token from local storage
-const getAuthToken = localStorage.getItem("token");
 
 export function ProtectedRoute ({ children }: { children: JSX.Element }) {
   const navigate = useNavigate();
-  if (getAuthToken != null) { 
+  if (localStorage.getItem("token")) { 
     console.log("Token found")
-
     return children;
-  }
+
+  } else {
   console.log("No token found")
 
   React.useEffect(() => {
     navigate("/")
   }, []);
+}
 };
 
 
