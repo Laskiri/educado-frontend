a89e755b494396e56c463f47400ae28aca9da627
pernilje <94057672+pernilje@users.noreply.github.com>
2023-11-09 14:09:38 +0100

    changing the create course UI

--- app/src/pages/CourseCreation.tsx
+++ app/src/pages/CourseCreation.tsx
@@ -3,6 +3,7 @@ import { Link, useParams} from 'react-router-dom'
 import { useForm, SubmitHandler, set } from 'react-hook-form'
 import { toast } from 'react-toastify'
 import useSWR from 'swr'
+import { Dropzone } from '../components/Dropzone/Dropzone';
 
 
 // Hooks
@@ -13,7 +14,7 @@ import { StorageFile } from '../interfaces/File'
 
 // Services
 import CourseServices from '../services/course.services'
-import StorageService from '../services/storage.services'
+import StorageServices from '../services/storage.services'
 
 // Pages
 import NotFound from './NotFound'
@@ -27,6 +28,9 @@ import { SectionForm } from '../components/dnd/SectionForm'
 // Icons
 import { ArrowLeftIcon } from '@heroicons/react/24/outline'
 import { boolean } from 'yup';
+import { getUserInfo } from '../helpers/userInfo';
+import { Navigate, useNavigate } from 'react-router-dom';
+
 
 import { BACKEND_URL } from "../helpers/environment";
 
@@ -52,7 +56,9 @@ interface Inputs {
  * @returns HTML Element
  */
 const CourseCreation = () => {
-  
+    const [isLoading, setIsLoading] = useState(false);
+    const [coverImage, setCoverImage] = useState(null);
+    const navigate = useNavigate();
 
   const token = 'dummyToken'
   // const token = useToken();
@@ -77,25 +83,7 @@ const CourseCreation = () => {
     }, []);
         
     
-    /**
-     * Extra function to handle the response from the course service before it is passed to the useSWR hook
-     * 
-     * @param url The url to fetch the course details from backend
-     * @param token The user token
-     * @returns The course details
-     */
-    const getData = async (url: string/*, token: string*/) => {
-        const res:any = await CourseServices.getCourseDetail(url/*, token*/)
-        
-        setStatusSTR(res.status);
-        return res;
-    }
-
-    // Fetch Course Details
-    const { data, error } = useSWR(
-        token ? [`${BACKEND_URL}/api/courses/${id}`, token] : null,
-        getData
-    )
+    
 
 //  // Fetch Categories
 //   const { data: categoriesData, error: categoriesError } = useSWR(
@@ -110,36 +98,28 @@ const { register, handleSubmit, formState: { errors } } = useForm<Inputs>()
  * Handles the form submission for updating a course's details.
  * @param {Inputs} data - The form data containing the updated course details.
 */
-const onSubmit: SubmitHandler<Inputs> = (data) => {
-    
-    let newStatus = statusSTR;
 
-    if(statusChange){
-        if(statusSTR === "draft"){
-            newStatus = "published";
-        }else{
-            newStatus = "draft";
-        }
-        setStatusChange(false);
-    }
-
-    if (confirm("Você tem certeza?") == true) {
-        const changes: Inputs = {
-            coverImg: data.coverImg,
-            title: data.title,
-            description: data.description,
-            category: data.category,
-            difficulty: data.difficulty,
-            status: newStatus,
-            estimatedHours: data.estimatedHours
-        }
-
-        // Update course details
-        CourseServices.updateCourseDetail(changes, id/*, token */)
-        .then(res => {toast.success('Curso atualizado'); setStatusSTR(changes.status);}) // Course updated
-        .catch(err => toast.error(err)) // Error updating course
-    }
-}
+// success on submit handler
+const onSubmit: SubmitHandler<Inputs> = async (data) => {
+    const { id } = getUserInfo();
+    setIsLoading(true);
+    CourseServices.createCourse({
+      title: data.title,
+      description: data.description,
+      category: data.category,
+      difficulty: data.difficulty,
+      estimatedHours: data.estimatedHours,
+      creator: id,
+        }, token)
+      .then(res => {
+        console.log(res);
+        StorageServices.uploadFile({ id: res.data._id + "/0", filePath: coverImage });
+        //CourseServices.updateCoverImage(res.data._id, token); // pass the required arguments
+        navigate(`/courses/edit/${res.data._id}`);
+      })
+      .catch(err => console.log(err))
+      .finally();
+  };
   
     /** TODO: Reimplement when buckets have been implemented */
     /* if (coverImg) {
@@ -172,6 +152,10 @@ const onSubmit: SubmitHandler<Inputs> = (data) => {
         }
     }
 
+    function returnFunction(coverImage: any) {
+        setCoverImage(coverImage);
+      }
+
 
     
   // TODO: update cover image function
@@ -196,140 +180,97 @@ const onSubmit: SubmitHandler<Inputs> = (data) => {
         } */
   }
 
-  if (error /* || categoriesError */) return <NotFound />
-  if (!data /* || !categories || (!data && !categories) */) return <Loading/>
-  
- 
 
   return (
         <Layout meta={`Course: ${id}`}>
 
-            {/** Course navigation */}
-            <form onSubmit={handleSubmit(onSubmit)} >
-                <div className="navbar bg-base-100 ">
-                    <div className='flex-1'>
-                        <Link to="/courses" className="btn btn-square btn-ghost normal-case text-xl" reloadDocument><ArrowLeftIcon width={24} /></Link>
-                        <a className="normal-case text-xl ml-4">{data.title}</a>
-                    </div>
-                    <div className="flex-none space-x-2">
-                        <button type="button" onClick={deleteCourse} className='left-0 std-button bg-warning hover:bg-red-800 ml-4' >Excluir</button> {/*Delete button*/}
-                        <button type="submit" className='std-button text-white border-0'>Atualizar</button> {/* Update button */}
-                        <button type="submit" onClick={() => setStatusChange(true)} className='std-button bg-primary text-white border-0'>{statusSTR === "draft"? "Publicar":"Definir como rascunho" }</button>
-                    </div>
-                </div>
-
-                {/** Course details edit */}
-                <div className="container mx-auto flex flex-row space-x-4 my-6">
-                    <div className='w-full max-w-5xl mx-auto bg-white rounded p-6'>
-                        <div className="grid grid-cols-1 lg:grid-cols-1 gap-6">
-                            <div className='flex flex-col space-y-6 divide'>
-
-                                {/* Course status */}
-                                <div className='flex flex-col justify-center pb-6'>
-                                  <h1 className='text-3xl text-center font-medium'>Curso</h1> {/* Course details */}
-                                  <div className='flex flex-row justify-center'>
-                                    <div className={'w-3 h-3 mx-2 rounded-full m-auto '+(statuses[statusSTR].color ?? statuses.default.color)} />
-                                    <p className='italic'>
-                                      {statuses[statusSTR].br ?? statuses.default.br}
-                                    </p>
-                                    
-                                  </div>
-                                </div>
-
-                                {/** Course Title Field */}
-                                <div className="flex flex-col space-y-2">
-                                    <label htmlFor='title'>Título</label>
-                                    <input type="text" defaultValue={data.title} placeholder={data.title}
-                                        className="form-field focus:outline-none focus:ring-2 focus:ring-blue-600 focus:border-transparent"
-                                        {...register('title', { required: true})}
-                                    />
-                                    {errors.title && <span>Este campo é obrigatório!</span>}
-                                </div>
-
-                                {/** Course Description Field */}
-                                <div className="flex flex-col space-y-2">
-                                    <label htmlFor='description'>Descrição</label>
-                                    <textarea rows={4} defaultValue={data.description} placeholder={data.description}
-                                        className="resize-none form-field focus:outline-none focus:ring-2 focus:ring-blue-600 focus:border-transparent"
-                                        {...register('description', { required: true })}
-                                    />
-                                    {errors.description && <span>Este campo é obrigatório!</span>}
-                                </div>
-
-                                {/* Field to choose a category from a list of options */}
-                                <div className="flex flex-col space-y-2 text-left">
-                                    <label htmlFor='category'>Categoria</label>
-                                    <select defaultValue={data.category}
-                                        className="form-field focus:outline-none focus:ring-2 focus:ring-primary focus:border-transparent"
-                                        {...register('category', { required: true })}
-                                    >
-                                        {/* Hard coded options by PO, should be changed to get from db */}
-                                        {categoriesOptions}
-                                    </select>
-                                    {errors.category && <span className='text-warning'>Este campo é obrigatório</span>}
-                                </div>
-
-                                {/* Field to select a level from a list of options */}
-                                <div className="flex flex-col space-y-2 text-left">
-                                    <label htmlFor='level'>Nível</label> {/* Level */}
-                                    <select defaultValue={data.difficulty}
-                                        className="small-form-field focus:outline-none focus:ring-2 focus:ring-primary focus:border-transparent"
-                                        {...register('difficulty', { required: true })}
-                                    >
-                                        {/* Hard coded options by PO, should be changed to get from db */}
-                                        <option value={1}>Iniciante </option> {/* Beginner */}
-                                        <option value ={2}>Intermediário</option> {/* Intermediate */}
-                                        <option value={3}>Avançado </option> {/* Advanced */}
-
-                                    </select>
-                                    {errors.difficulty && <span className='text-warning'>Este campo é obrigatório</span>}
-                                </div>
-
-                                {/* Field to input the estimated estimatedHours */}
-                                <div className="flex flex-col space-y-2 text-left">
-                                    <label htmlFor='title'>Tempo estimado</label> {/* Estimated time */}
-                                    <input type="number" defaultValue={data.estimatedHours} min={0} step={1}
-                                        className="form-field focus:outline-none focus:ring-2 focus:ring-primary focus:border-transparent"
-                                        {...register('estimatedHours', { required: true })}
-                                    />
-                                    {errors.title && <span className='text-warning'>Este campo é obrigatório</span>}
-                                </div>
-
-                                {/** Cover Image Field */}
-                                <div className="flex flex-col">
-                                    <div className='relative'>
-                                        <div className='p-0 rounded-b-none rounded-t border-gray-300 border-x border-t h-[240px] overflow-hidden'>
-                                            {data.coverImg ?
-                                                <img src={data.coverImg} alt={data.title} className="w-full h-max rounded object-cover" /> :
-                                                <div className='h-full w-full oceanic-gradient flex justify-center items-center text-2xl text-white'>Sem imagem de capa</div>
-                                            }
-
-                                        </div>
-                                        {/* Cover image upload */}
-                                        <input type="file" accept='.jpg,.jpeg,.png'
-                                            {...register('coverImg')}
-                                            // onChange={onCoverImgChange}
-                                            className='file-input w-full input-bordered rounded-b rounded-t-none focus:outline-none'
-                                        >
-                                        </input>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </form>
-
-            <div className="container mx-auto flex flex-row space-x-4 my-6">
-                <div className='w-full max-w-5xl mx-auto bg-white rounded p-6'>
-                    {/** Course Sections area  */}
-                    <div className='flex flex-col space-y-2 divide'>
-                        <h1 className='text-xl font-medium mb-4'>Seções do curso</h1>
-                        <SectionForm/>
-                        <SectionList sections={data.sections} />
-                    </div>
-                </div>
+           {/*Field to input the title of the new course*/}
+          <form className="flex h-full flex-col justify-between space-y-4" onSubmit={handleSubmit(onSubmit)}>
+            <div className="flex flex-col space-y-2 text-left">
+              <label htmlFor='title'>Título</label> {/*Title*/}
+              <input type="text" defaultValue={""}
+                className="form-field focus:outline-none focus:ring-2 focus:ring-primary focus:border-transparent"
+                {...register("title", { required: true })}
+              />
+              {errors.title && <span className='text-warning'>Este campo é obrigatório</span>} {/** This field is required */}
             </div>
+
+            {/*Field to choose a category from a list of options*/}
+            <div className="flex flex-col space-y-2 text-left">
+              <label htmlFor='category'>Categoria</label> {/** Category */}
+              <select
+                defaultValue={"personal finance"}
+                className="form-field focus:outline-none focus:ring-2 focus:ring-primary focus:border-transparent"
+                {...register("category", { required: true })}
+              >
+                {/*Hard coded options by PO, should be changed to get from db*/}
+                {categoriesOptions}
+
+              </select>
+              {errors.description && <span className='text-warning'>Este campo é obrigatório</span>} {/** This field is required */}
+            </div>
+
+            <div className="flex items-center gap-8 w-full mt-8">
+
+              {/*Cover image field is made but does not interact with the db*/}
+              <div className="flex flex-col space-y-2 text-left">
+                <label htmlFor='cover-image'>Imagem de capa</label> {/** Cover image */}
+                <Dropzone callBack={returnFunction}></Dropzone> {/** FIX: Doesn't have the functionality to upload coverimage to Buckets yet!*/}
+                {errors.description && <span className='text-warning'>Este campo é obrigatório</span>} {/** This field is required */}
+              </div>
+
+              {/*Field to select a level from a list of options*/}
+              <div className="flex flex-col space-y-2 text-left">
+                <label htmlFor='level'>Nível</label> {/** Level */}
+                <select
+                  className="small-form-field focus:outline-none focus:ring-2 focus:ring-primary focus:border-transparent"
+                  {...register("difficulty", { required: true })}
+                >
+                  {/*Hard coded options by PO, should be changed to get from db*/}
+                  <option value={1}>Iniciante </option> {/** Beginner */}
+                  <option value={2}>Intermediário</option> {/** Intermediate */}
+                  <option value={3}>Avançado </option> {/** Advanced */}
+
+                </select>
+                {errors.difficulty && <span className='text-warning'>Este campo é obrigatório</span>} {/** This field is required */}
+              </div>
+
+              {/*Field to input the estimated time*/}
+              <div className="flex flex-col space-y-2 text-left">
+                <label htmlFor='title'>Tempo estimado</label> {/** Estimated time */}
+                <input type="number" defaultValue={""} min={0} step={1}
+                  className="form-field focus:outline-none focus:ring-2 focus:ring-primary focus:border-transparent"
+                  {...register("estimatedHours", { required: true })}
+                />
+                {errors.title && <span className='text-warning'>Este campo é obrigatório</span>} {/** This field is required */}
+              </div>
+
+            </div>
+
+            {/*Field to input the description of the course*/}
+            <div className="flex flex-col space-y-2 text-left">
+              <label htmlFor='description'>Descrição</label> {/** Description */}
+              <textarea rows={4} defaultValue={""}
+                className="resize-none form-field focus:outline-none focus:ring-2 focus:ring-primary focus:border-transparent"
+                {...register("description", { required: true })}
+              />
+              {errors.description && <span className='text-warning'>Este campo é obrigatório</span>} {/** This field is required */}
+            </div>
+
+            {/*Create and cancel buttons*/}
+            <div className='modal-action'>
+              <div className="flex items-center justify-between gap-4 w-full mt-8">
+                <label htmlFor='course-create' className=" bg-primary hover:bg-primaryHover border border-primary focus:ring-blue-500 focus:ring-offset-blue-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded">
+                  <button type="submit" className='py-2 px-4 h-full w-full'>
+                    Criar {/** Create */}
+                  </button>
+                </label>
+                <label htmlFor='course-create' className="py-2 px-4 bg-white hover:bg-gray-100 border border-primary  hover:border-primaryHover hover:text-primaryHover  text-primary w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded">
+                  Cancelar {/** Cancel */}
+                </label>
+              </div>
+            </div>
+          </form>
 
         </Layout>
   )
