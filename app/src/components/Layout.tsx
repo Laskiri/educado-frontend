e3eda5aa1c4f98efb9d283a7618940385d1de7dd
Slovenly <54136932+SlovenlyTF@users.noreply.github.com>
2023-11-20 14:44:49 +0100

    Indent error

--- app/src/components/Layout.tsx
+++ app/src/components/Layout.tsx
@@ -13,29 +13,29 @@ import { Navbar } from './Navbar';
  */
 const Layout = ({ children }: { children: Array<ReactNode> | ReactNode, meta: string | undefined }) => {
 	return <>
-			<div className="flex w-screen h-screen text-gray-700">
-				<div className="flex flex-col flex-grow">
-					{/** Top Nav bar */}
-					<Navbar />
+		<div className="flex w-screen h-screen text-gray-700">
+			<div className="flex flex-col flex-grow">
+				{/** Top Nav bar */}
+				<Navbar />
 
-					{/** Content */}
-					<main className="flex-grow overflow-x-hidden bg-gradient-to-br from-[#c8e5ec] to-[white]">
-						{children}
-					</main>
-				</div>
+				{/** Content */}
+				<main className="flex-grow overflow-x-hidden bg-gradient-to-br from-[#c8e5ec] to-[white]">
+					{children}
+				</main>
 			</div>
-			<ToastContainer
-				position="top-right"
-				autoClose={5000}
-				hideProgressBar={false}
-				newestOnTop={false}
-				closeOnClick
-				rtl={false}
-				pauseOnFocusLoss
-				draggable
-				pauseOnHover={false}
-				theme="light"
-			/>
+		</div>
+		<ToastContainer
+			position="top-right"
+			autoClose={5000}
+			hideProgressBar={false}
+			newestOnTop={false}
+			closeOnClick
+			rtl={false}
+			pauseOnFocusLoss
+			draggable
+			pauseOnHover={false}
+			theme="light"
+		/>
 	</>
 }
 
