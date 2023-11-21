0e29069fbd579c9bf86dc57eb8f033bc2f483dcb
AdachiGayming <benjaminspeyerlarsen@gmail.com>
2023-11-21 22:40:38 +0100

    Fixed navbar

--- app/src/components/Layout.tsx
+++ app/src/components/Layout.tsx
@@ -3,8 +3,7 @@ import { ReactNode } from 'react'
 import { ToastContainer } from 'react-toastify';
 
 // Components
-import RequireAuth from './RequireAuth'
-import Navbar from './Navbar';
+import { Navbar } from './Navbar';
 
 /**
  * Layout component
@@ -13,41 +12,31 @@ import Navbar from './Navbar';
  * @returns HTML Element
  */
 const Layout = ({ children }: { children: Array<ReactNode> | ReactNode, meta: string | undefined }) => {
-	return (
-		<RequireAuth>
-			<div className="flex w-screen h-screen text-gray-700">
-				<div className="flex flex-col flex-grow">
-					{/** Top Nav bar */}
-					<Navbar />
+	return <>
+		<div className="flex w-screen h-screen text-gray-700">
+			<div className="flex flex-col flex-grow">
+				{/** Top Nav bar */}
+				<Navbar />
 
-					{/** Content */}
-					<main className="flex-grow overflow-x-hidden bg-gradient-to-br from-[#c8e5ec] to-[white]">
-						{children}
-					</main>
-          
-          {/*
-					<footer className="footer footer-center p-4 bg-base-100 border-t text-base-content">
-						<div>
-							<p>Copyright © {new Date().getFullYear()} - All rights reserved by Educado</p>
-						</div>
-					</footer>
-          */}
-				</div>
+				{/** Content */}
+				<main className="flex-grow overflow-x-hidden bg-gradient-to-br from-[#c8e5ec] to-[white]">
+					{children}
+				</main>
 			</div>
-			<ToastContainer
-				position="top-right"
-				autoClose={5000}
-				hideProgressBar={false}
-				newestOnTop={false}
-				closeOnClick
-				rtl={false}
-				pauseOnFocusLoss
-				draggable
-				pauseOnHover={false}
-				theme="light"
-			/>
-		</RequireAuth>
-	)
+		</div>
+		<ToastContainer
+			position="top-right"
+			autoClose={5000}
+			hideProgressBar={false}
+			newestOnTop={false}
+			closeOnClick
+			rtl={false}
+			pauseOnFocusLoss
+			draggable
+			pauseOnHover={false}
+			theme="light"
+		/>
+	</>
 }
 
 export default Layout
