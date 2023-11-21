cea901b8539802ce25f1b2164104cba8090acdf3
AdachiGayming <benjaminspeyerlarsen@gmail.com>
2023-11-21 18:17:05 +0100

    Fixed tests, again
    I am so tired of these tests not being so indecisive about their outcome

--- app/src/components/Layout.tsx
+++ app/src/components/Layout.tsx
@@ -3,10 +3,8 @@ import { ReactNode } from 'react'
 import { ToastContainer } from 'react-toastify';
 
 // Components
-
 import RequireAuth from './RequireAuth'
 import Navbar from './Navbar';
-
 
 /**
  * Layout component
@@ -18,6 +16,7 @@ const Layout = ({ children }: { children: Array<ReactNode> | ReactNode, meta: st
 	return (
 		<RequireAuth>
 			<div className="flex w-screen h-screen text-gray-700">
+				<div className="flex flex-col flex-grow">
 					{/** Top Nav bar */}
 					<Navbar />
 
@@ -25,22 +24,30 @@ const Layout = ({ children }: { children: Array<ReactNode> | ReactNode, meta: st
 					<main className="flex-grow overflow-x-hidden bg-gradient-to-br from-[#c8e5ec] to-[white]">
 						{children}
 					</main>
-
+          
+          {/*
+					<footer className="footer footer-center p-4 bg-base-100 border-t text-base-content">
+						<div>
+							<p>Copyright © {new Date().getFullYear()} - All rights reserved by Educado</p>
+						</div>
+					</footer>
+          */}
+				</div>
 			</div>
-		</div>
-		<ToastContainer
-			position="top-right"
-			autoClose={5000}
-			hideProgressBar={false}
-			newestOnTop={false}
-			closeOnClick
-			rtl={false}
-			pauseOnFocusLoss
-			draggable
-			pauseOnHover={false}
-			theme="light"
-		/>
-	</>
+			<ToastContainer
+				position="top-right"
+				autoClose={5000}
+				hideProgressBar={false}
+				newestOnTop={false}
+				closeOnClick
+				rtl={false}
+				pauseOnFocusLoss
+				draggable
+				pauseOnHover={false}
+				theme="light"
+			/>
+		</RequireAuth>
+	)
 }
 
 export default Layout
