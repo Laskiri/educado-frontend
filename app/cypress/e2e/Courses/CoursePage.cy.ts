be52bb49bc795735232eb94ac1eeab8340fb9283
Slovenly <54136932+SlovenlyTF@users.noreply.github.com>
2023-11-24 14:18:59 +0100

    Update CoursePage.cy.ts

--- app/cypress/e2e/Courses/CoursePage.cy.ts
+++ app/cypress/e2e/Courses/CoursePage.cy.ts
@@ -1,34 +1,55 @@
+import "cypress-localstorage-commands";
+
 const BACKEND_URL = Cypress.env('BACKEND_URL')
-const ID = '654a188604774633a8d7abc8'
 
 describe('Tooltip on Course Page', () => {
 
   before(() => {
-    before(() => {
-      cy.intercept('POST', `${BACKEND_URL}/api/auth/login`, {
-        statusCode: 202,
-        body: {
-          userInfo: {
-            id: '1',
-            name: 'Test User',
-            email: 'test@email.com'
-          },
-          token: 'testToken'
+    cy.intercept('POST', `${BACKEND_URL}/api/auth/login`, {
+      statusCode: 202,
+      body: {
+        userInfo: {
+          id: '1',
+          name: 'Test User',
+          email: 'test@email.com'
         },
-      });
-  
-      cy.visit('http://localhost:3000/login')
-      cy.get('#email-field').type('test@email.com')
-      cy.get('#password-field').type('password')
-      cy.get('#submitLoginButton').click()
-      cy.url().should('include', '/courses')
-      cy.saveLocalStorage();
+        token: 'testToken'
+      },
     });
+
+    cy.visit('http://localhost:3000/login')
+    cy.get('#email-field').type('test@email.com')
+    cy.get('#password-field').type('password')
+    cy.get('#submitLoginButton').click()
+    cy.url().should('include', '/courses')
+    cy.saveLocalStorage();
   });
 
+
   beforeEach(() => {
-		cy.restoreLocalStorage();
-    cy.visit(`http://localhost:3000/courses/edit/${ID}`)
+    cy.intercept('GET', `${BACKEND_URL}/api/courses/*`, {
+      statusCode: 200,
+      body: {
+        _id: 1,
+        title: "Test Course",
+        category: "sewing",
+        coverImg: null,
+        description: "This is a test course.",
+        creator: "Test User",
+        status: "published",
+        estimatedHours: 0,
+        rating: 1,
+        difficulty: 1,
+        numOfSubscriptions: 0,
+        dateUpdated: Date.now(),
+        dateCreated: Date.now(),
+        sections: [],
+        __v: 2
+      },
+    });
+
+    cy.visit(`http://localhost:3000/courses/edit/1`)
+    cy.restoreLocalStorage();
   });
 
 
@@ -51,6 +72,7 @@ describe('Tooltip on Course Page', () => {
     cy.get('#tooltipClose').click()
     cy.get('#tooltipBox').should('not.exist')
   });
+
 
   it('Finish tooltip', () => {
     cy.get('#tooltipIcon1').click()
