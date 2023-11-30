aa002bfeffcdaae3b111264b5aa4529d4d138d55
magnemo <magne@skoven7.dk>
2023-11-30 20:28:59 +0100

    It works i promise

--- app/src/components/SectionCreation.tsx
+++ app/src/components/SectionCreation.tsx
@@ -16,6 +16,7 @@ import { useNavigate } from 'react-router-dom';
 
 import Loading from './general/Loading'
 import Layout from './Layout'
+import { is } from 'cypress/types/bluebird';
 
 interface Inputs {
     id: string,
@@ -28,7 +29,9 @@ interface Inputs {
 export const SectionCreation = ({ id, token, setTickChange}: Inputs ) => {
 
 
+  const [isLeaving, setIsLeaving] = useState<boolean>(false);
   const [onSubmitSubscribers, setOnSubmitSubscribers] = useState<Function[]>([]);
+
   const navigate = useNavigate();
   function addOnSubmitSubscriber(callback: Function) {
     //console.log("add subscriber");
@@ -53,6 +56,14 @@ export const SectionCreation = ({ id, token, setTickChange}: Inputs ) => {
 
   function onSubmit() {
     notifyOnSubmitSubscriber();
+
+    setIsLeaving((isLeaving) =>{
+      if(isLeaving && confirm("Tem certeza que deseja sair? Você perderá todas as alterações feitas.")){
+        window.location.href = '/courses';
+      }
+      return false;
+    });
+    
     //setTickChange(2)(); //TODO: add in when next page is implemented
   }
 
@@ -108,17 +119,17 @@ export const SectionCreation = ({ id, token, setTickChange}: Inputs ) => {
           {/*Create and cancel buttons*/}
           <div className='className="flex w-full float-right space-y-4 "'>
             <div className="flex items-center justify-between gap-4 w-full mt-8">
-              <label onClick={()=>changeTick(0)}  htmlFor='course-create' className=" w-full cursor-pointer underline py-2 px-4 bg-transparent hover:bg-warning-100 text-primary w-full transition ease-in duration-200 text-center text-base font-semibold focus:outline-none focus:ring-2 focus:ring-offset-2  rounded">
+              <label onClick={()=>changeTick(0)}   className=" w-full cursor-pointer underline py-2 px-4 bg-transparent hover:bg-warning-100 text-primary w-full transition ease-in duration-200 text-center text-base font-semibold focus:outline-none focus:ring-2 focus:ring-offset-2  rounded">
                   Voltar para Informações {/** GO BACK TO COURSE CREATION PAGE 1/3 IN THE CHECKLIST */}
               </label>
 
-              <label htmlFor='course-create' className="pl-56 underline py-2 bg-transparent hover:bg-primary-100 text-primary w-full transition ease-in duration-200 text-center text-base font-semibold focus:outline-none focus:ring-2 focus:ring-offset-2  rounded">
-                <label onClick={onSubmit} className='underline' >
+              <label  className="pl-56  underline py-2 bg-transparent hover:bg-primary-100 text-primary w-full transition ease-in duration-200 text-center text-base font-semibold focus:outline-none focus:ring-2 focus:ring-offset-2  rounded">
+                <label onClick={()=>{setIsLeaving(true); onSubmit()}} className='hover:cursor-pointer underline' >
                 Salvar como Rascunho {/** Save as draft */}
                 </label>
               </label>
 
-              <label htmlFor='course-create' className="h-12 p-2 bg-primary hover:bg-primary focus:ring-blue-500 focus:ring-offset-blue-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded-lg">
+              <label  className="h-12 p-2 bg-primary hover:bg-primary focus:ring-blue-500 focus:ring-offset-blue-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded-lg">
                 <label onClick={onSubmit} className='py-2 px-4 h-full w-full cursor-pointer' >
                     Revisar Curso {/** Go to the Revisar curso page which is not yet implemented therefore it is just a save  */}
                 </label>
