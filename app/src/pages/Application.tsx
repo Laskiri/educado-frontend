a55ef52c254f11d3a299467042f0705128c864cd
Martin <maje22@student.aau.dk>
2024-09-30 16:53:12 +0200

    Confirmation popup added to application form (WIP)
    Still needs finetuning of styling and completion of functionality.
    
    Co-Authored-By: MikailGuzel <113025194+MikailGuzel@users.noreply.github.com>
    Co-Authored-By: JakobT97 <94533850+JakobT97@users.noreply.github.com>
    Co-Authored-By: sigurdavh <sigurdavh@hotmail.com>

--- app/src/pages/Application.tsx
+++ app/src/pages/Application.tsx
@@ -11,7 +11,18 @@ import Motivation from '../components/Application/Motivation';
 import AcademicExperience from '../components/Application/AcademicExperience';
 import WorkExperience from '../components/Application/ProfessionalExperience';
 
+// Confirmation popup component
+import React, { useState } from 'react';
+import PopupComponent from '../components/PopupComponent';
+
 const Application = () => {
+
+  // State for the confirmation popup visibility
+  const [isPopupVisible, setIsPopupVisible] = useState(false);
+
+  // Functions to open and close the confirmation popup
+  const openPopup = () => setIsPopupVisible(true);
+  const closePopup = () => setIsPopupVisible(false);
 
   //Get id from URL
   const { id } = useParams();
@@ -43,51 +54,64 @@ const Application = () => {
     })
   };
 
-return (
-<main className="flex flex-col min-h-screen bg-gradient-to-br from-[#C9E5EC] 0% to-[#FFF] 100%" >
-  
-  { /* Navbar */ }
-  <nav className="flex fixed w-full items-center justify-between bg-secondary box-shadow-md bg-fixed top-0 left-0 right-0 z-50 bg-F1F9FB shadow-[0px 4px 4px 0px rgba(35, 100, 130, 0.25)]">
-    <div className="w-[165.25px] h-6 justify-start items-center gap-[7.52px] flex py-6 px-12">
-      <div className="navbar-start">
-        <Link to="/" className="w-[165.25px] h-6 justify-start items-center gap-[6px] inline-flex space-x-1 normal-case text-xl">
-          <img src= '/logo.svg' alt="logo" className="w-[24.43px] h-6" /> <img src= '/educado.svg' alt="educado" className="h-6" />
-        </Link>
-      </div>
-    </div>
-  </nav>
-  
-  <body className="relative right-0 h-full flex-1 flex flex-col z-10">
-    <form className="relative right-0 w-full overflow-none flex-1 h-screen flex flex-col items-center gap-5 z-50" onSubmit={handleSubmit(onSubmit)}>
-   
-      {/* Box for text */}
-      <div className="items-center p-10 pt-20">
-        <h1 className="text-center text-cyan-800 text-[32px] font-bold font-['Montserrat']">
-          Que bom que você quer fazer parte do Educado! {/* Glad you want to be part of Educado! */}
-        </h1>
-        <p className="text-center text-neutral-700 text-lg font-normal font-['Montserrat']">
-          {/* We need some information to approve your content creator access. We'll get back to you with an answer via e-mail. */}
-          Precisamos de algumas informações para aprovar seu acesso de criador de conteúdo. Retornaremos com uma resposta via e-mail
-        </p>
-      </div>
-    
-      <Motivation register={register} errors={errors} />
-      <AcademicExperience register={register} errors={errors} />
-      <WorkExperience register={register} errors={errors} />
-      
+  return (
+    <main className="flex flex-col min-h-screen bg-gradient-to-br from-[#C9E5EC] 0% to-[#FFF] 100%" >
 
-      {/* Box for Professional Experience */}
-      
+      { /* Navbar */ }
+      <nav className="flex fixed w-full items-center justify-between bg-secondary box-shadow-md bg-fixed top-0 left-0 right-0 z-50 bg-F1F9FB shadow-[0px 4px 4px 0px rgba(35, 100, 130, 0.25)]">
+        <div className="w-[165.25px] h-6 justify-start items-center gap-[7.52px] flex py-6 px-12">
+          <div className="navbar-start">
+            <Link to="/" className="w-[165.25px] h-6 justify-start items-center gap-[6px] inline-flex space-x-1 normal-case text-xl">
+              <img src= '/logo.svg' alt="logo" className="w-[24.43px] h-6" /> <img src= '/educado.svg' alt="educado" className="h-6" />
+            </Link>
+          </div>
+        </div>
+      </nav>
 
-      <div className="w-[65%] flex justify-end">
-        <button type="submit" className="w-[238px] h-[52px] px-10 py-4 bg-cyan-800 hover:bg-cyan-900 rounded-lg justify-center items-start gap-2.5 inline-flex text-center text-white text-lg font-bold font-['Montserrat']">
-            Enviar para análise {/* Send for analysis */}
-        </button>
-      </div>
-    </form>
-  </body>
-</main>
-)    
+      <body className="relative right-0 h-full flex-1 flex flex-col z-10">
+        <form className="relative right-0 w-full overflow-none flex-1 h-screen flex flex-col items-center gap-5 z-50" onSubmit={handleSubmit(onSubmit)}>
+
+          {/* Box for text */}
+          <div className="items-center p-10 pt-20">
+            <h1 className="text-center text-cyan-800 text-[32px] font-bold font-['Montserrat']">
+              Que bom que você quer fazer parte do Educado! {/* Glad you want to be part of Educado! */}
+            </h1>
+            <p className="text-center text-neutral-700 text-lg font-normal font-['Montserrat']">
+              {/* We need some information to approve your content creator access. We'll get back to you with an answer via e-mail. */}
+              Precisamos de algumas informações para aprovar seu acesso de criador de conteúdo. Retornaremos com uma resposta via e-mail
+            </p>
+          </div>
+
+          <Motivation register={register} errors={errors} />
+          <AcademicExperience register={register} errors={errors} />
+          <WorkExperience register={register} errors={errors} />
+
+
+          {/* Box for Professional Experience */}
+
+
+          <div className="w-[65%] flex justify-end">
+            {/* Send for analysis */}
+            {/* TODO: fix text not fitting in button */}
+            <button onClick={openPopup}
+                    className="w-[238px] h-[52px] px-10 py-4 bg-cyan-800 hover:bg-cyan-900 rounded-lg justify-center items-start gap-2.5 inline-flex text-center text-white text-lg font-bold font-['Montserrat']">
+              Enviar para análise
+            </button>
+
+            {/* Confirmation Popup */}
+            <PopupComponent
+                title="Enviar para análise"
+                contentText={"Você tem certeza que deseja enviar o formulário de aplicação? Essa ação não pode ser desfeita."}
+                confirmBtnText={"Confirmar"}
+                onConfirm={handleSubmit(onSubmit)}
+                onClose={closePopup}
+                isVisible={isPopupVisible}
+            />
+          </div>
+        </form>
+      </body>
+    </main>
+  )
 }
 
 export default Application
