e89bb7d64c48eb67f0e78de8534581e53ca1ce20
sourtendon <113062702+sourtendon@users.noreply.github.com>
2024-10-02 15:25:29 +0200

    Added visuals for buttons: To toggle admin role for users To view user details To delete a user

--- app/src/pages/EducadoAdmin.tsx
+++ app/src/pages/EducadoAdmin.tsx
@@ -107,7 +107,12 @@ const EducadoAdmin = () => {
                                     <tr key={key} className="px-5 py-5 border-b border-gray-200 bg-white text-base font-['Montserrat']">
                                         <td>
                                             <p className="text-gray-900 whitespace-no-wrap ml-4" id="admin">
-                                                Slider
+                                                <div className="toggle-switch ">
+                                                    <label className="inline-flex items-center cursor-pointer">
+                                                        <input type="checkbox" value="" className="sr-only peer"/>
+                                                            <div className="relative w-10 h-3.5 bg-gray-200 peer-focus:outline-none peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-300 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[-3.5px] after:start-[2px] after:bg-gray-400 peer-checked:after:bg-cyan-800  after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-toggleChecked"></div>
+                                                    </label>
+                                                </div>
                                             </p>
                                         </td>
                                         <td>
@@ -134,10 +139,24 @@ const EducadoAdmin = () => {
                                                 {formatDate(application.joinedAt)}
                                             </p>
                                         </td>
-                                        <td className="flex items-center p-4">
-                                            <Link to={`${location.pathname}/${application._id}`} id="viewDetails" className="flex items-center justify-center p-2 w-full bg-[#166276] rounded font-semibold text-base text-white">
-                                                Ver detalhes
-                                            </Link>
+                                        <td>
+                                            <div className="flex items-center p-4">
+                                                <Link to={`${location.pathname}/${application._id}`} id="viewDetails" className="flex items-center justify-center p-4  bg-[#166276] rounded-full font-semibold text-base text-white">
+                                                    <svg className="shrink-0 size-3.5" width="15" height="15" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="3" stroke-linecap="round" stroke-linejoin="round">
+                                                        <path stroke-linecap="round" stroke-linejoin="round" d="M2.036 12.322a1.012 1.012 0 0 1 0-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178Z" />
+                                                    <path stroke-linecap="round" stroke-linejoin="round" d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
+                                                    </svg>
+                                                </Link>
+                                            </div>
+                                        </td>
+                                        <td>
+                                            <div className="flex items-center p-4">
+                                                <Link to={`${location.pathname}/${application._id}`} id="viewDetails" className="flex items-center justify-center p-4  bg-[#166276] rounded-full font-semibold text-base text-white">
+                                                    <svg className="shrink-0 size-3.5" width="15" height="15" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="3" stroke-linecap="round" stroke-linejoin="round">
+                                                        <path stroke-linecap="round" stroke-linejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
+                                                    </svg>
+                                                </Link>
+                                            </div>
                                         </td>
                                     </tr>
                                 );
