77ae5476422bb038dcb5276dabf478d904417a62
LazarShockX <donaldbr12@gmail.com>
2024-10-03 11:05:43 +0200

    Added width and break-word for each column to prevent i.e. long names from increasing page width, resulting in horizontal scrolling.

--- app/src/pages/EducadoAdmin.tsx
+++ app/src/pages/EducadoAdmin.tsx
@@ -58,8 +58,10 @@ const EducadoAdmin = () => {
                     <form className="flex flex-col md:flex-row w-3/4 md:w-full max-w-full md:space-x-4 space-y-3 md:space-y-0 justify-end p-6 -mt-4">
                         <select className="block bg-white min-w-[175px] flex-grow-0 border border-slate-300 rounded-md py-2 pr-3 shadow-sm focus:outline-none hover:bg-white focus:border-sky-500 focus:ring-1 sm:text-sm">
                             <option value="option1">Mais recentes</option>
+                            {/* 
                             <option value="option2">Option 2</option>
-                            <option value="option3">Option 3</option>
+                            <option value="option3">Option 3</option> 
+                            */}
                         </select>
                         <div className="relative min-w-[225px] flex-grow-0">
                             <input
@@ -88,12 +90,12 @@ const EducadoAdmin = () => {
 
                     <table className="min-w-full leading-normal">
                         <thead>
-                            <tr className="bg-white border-b border-gray-200 text-[#166276] text-left text-base font-base font-['Lato']">
-                                <th scope="col" className="p-5">Admin</th>
-                                <th scope="col" className="p-5">Nome</th>
-                                <th scope="col" className="p-5">Email</th>
-                                <th scope="col" className="p-5">Status</th>
-                                <th scope="col" className="p-5">Enviado em</th>
+                            <tr className="bg-white border-b-4 border-[#166276] text-[#166276] text-left text-base font-base font-['Lato']]">
+                            <th scope="col" className="p-7" style={{ width: '10%' }}>Admin</th>
+                            <th scope="col" className="p-5" style={{ width: '20%' }}>Nome</th>
+                            <th scope="col" className="p-5" style={{ width: '25%' }}>Email</th>
+                            <th scope="col" className="p-5" style={{ width: '20%' }}>Status</th>
+                            <th scope="col" className="p-5" style={{ width: '30%' }}>Enviado em</th>
                             </tr>
                         </thead>
                         <tbody>
@@ -106,7 +108,7 @@ const EducadoAdmin = () => {
                                 return (
                                     <tr key={key} className="px-5 py-5 border-b border-gray-200 bg-white text-base font-['Montserrat']">
                                         <td>
-                                            <p className="text-gray-900 whitespace-no-wrap ml-4" id="admin">
+                                            <p className="text-gray-900 whitespace-no-wrap ml-7" id="admin">
                                                 <div className="toggle-switch ">
                                                     <label className="inline-flex items-center cursor-pointer">
                                                         <input type="checkbox" value="" className="sr-only peer"/>
@@ -118,20 +120,20 @@ const EducadoAdmin = () => {
                                         <td>
                                             <div className="flex items-center">
                                                 <div className="ml-3">
-                                                    <p className="text-gray-900 whitespace-no-wrap" id="name">
+                                                    <p className="text-gray-900 whitespace-no-wrap" id="name" style={{ wordBreak: 'break-word'}}>
                                                         {application.firstName} {application.lastName}
                                                     </p>
                                                 </div>
                                             </div>
                                         </td>
-                                        <td>
+                                        <td style={{ wordBreak: 'break-word' }}>
                                             <p className="text-gray-900 whitespace-no-wrap" id="email">
                                                 {application.email}
                                             </p>
                                         </td>
                                         <td>
                                             <p className="text-gray-900 whitespace-no-wrap" id="status">
-                                                Status
+                                                Aguardando análise
                                             </p>
                                         </td>
                                         <td>
@@ -141,18 +143,17 @@ const EducadoAdmin = () => {
                                         </td>
                                         <td>
                                             <div className="flex items-center p-4">
-                                                <Link to={`${location.pathname}/${application._id}`} id="viewDetails" className="flex items-center justify-center p-4  bg-[#166276] rounded-full font-semibold text-base text-white">
+                                                <Link to={`${location.pathname}/${application._id}`} id="viewDetails" className="flex items-center justify-center p-4 bg-[#166276] rounded-full font-semibold text-base text-white">
                                                     <svg className="shrink-0 size-3.5" width="15" height="15" viewBox="0 0 24 24" fill="white" stroke="currentColor" stroke-width="1" stroke-linecap="round" stroke-linejoin="round">
                                                             <path d="M12 15a3 3 0 1 0 0-6 3 3 0 0 0 0 6Z" />
                                                             <path fill-rule="evenodd" d="M1.323 11.447C2.811 6.976 7.028 3.75 12.001 3.75c4.97 0 9.185 3.223 10.675 7.69.12.362.12.752 0 1.113-1.487 4.471-5.705 7.697-10.677 7.697-4.97 0-9.186-3.223-10.675-7.69a1.762 1.762 0 0 1 0-1.113ZM17.25 12a5.25 5.25 0 1 1-10.5 0 5.25 5.25 0 0 1 10.5 0Z" clip-rule="evenodd" />
-
                                                     </svg>
                                                 </Link>
                                             </div>
                                         </td>
                                         <td>
                                             <div className="flex items-center p-4">
-                                                <Link to={`${location.pathname}/${application._id}`} id="viewDetails" className="flex items-center justify-center p-4  bg-[#166276] rounded-full font-semibold text-base text-white">
+                                                <Link to={`${location.pathname}/${application._id}`} id="viewDetails" className="flex items-center justify-center p-4 -ml-4 bg-[#166276] rounded-full font-semibold text-base text-white">
                                                     <svg className="shrink-0 size-3.5" width="15" height="15" viewBox="0 0 24 24" fill="white" stroke="currentColor" stroke-width="1" stroke-linecap="round" stroke-linejoin="round">
                                                             <path fill-rule="evenodd" d="M16.5 4.478v.227a48.816 48.816 0 0 1 3.878.512.75.75 0 1 1-.256 1.478l-.209-.035-1.005 13.07a3 3 0 0 1-2.991 2.77H8.084a3 3 0 0 1-2.991-2.77L4.087 6.66l-.209.035a.75.75 0 0 1-.256-1.478A48.567 48.567 0 0 1 7.5 4.705v-.227c0-1.564 1.213-2.9 2.816-2.951a52.662 52.662 0 0 1 3.369 0c1.603.051 2.815 1.387 2.815 2.951Zm-6.136-1.452a51.196 51.196 0 0 1 3.273 0C14.39 3.05 15 3.684 15 4.478v.113a49.488 49.488 0 0 0-6 0v-.113c0-.794.609-1.428 1.364-1.452Zm-.355 5.945a.75.75 0 1 0-1.5.058l.347 9a.75.75 0 1 0 1.499-.058l-.346-9Zm5.48.058a.75.75 0 1 0-1.498-.058l-.347 9a.75.75 0 0 0 1.5.058l.345-9Z" clip-rule="evenodd" />
                                                     </svg>
