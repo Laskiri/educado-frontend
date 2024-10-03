dfc032d2ca519e28ed233bbeb8876b5ebadf136c
sourtendon <113062702+sourtendon@users.noreply.github.com>
2024-10-03 13:43:18 +0200

    Removed the extra scroll bar

--- app/src/pages/EducadoAdmin.tsx
+++ app/src/pages/EducadoAdmin.tsx
@@ -111,7 +111,7 @@ const EducadoAdmin = () => {
                                             <p className="text-gray-900 whitespace-no-wrap ml-7" id="admin">
                                                 <div className="toggle-switch ">
                                                     <label className="inline-flex items-center cursor-pointer">
-                                                        <input type="checkbox" value="" className="sr-only peer"/>
+                                                        <input type="checkbox" value="" className="hidden peer"/>
                                                             <div className="relative w-10 h-3.5 bg-gray-200 peer-focus:outline-none peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-300 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[-3.5px] after:start-[2px] after:bg-gray-400 peer-checked:after:bg-cyan-800  after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-toggleChecked"></div>
                                                     </label>
                                                 </div>
