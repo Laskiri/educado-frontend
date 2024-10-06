835b97006a1e27956afeae95f706ce0ded47683d
Frederik Woloszynski <116066516+BabaWolo@users.noreply.github.com>
2024-10-06 19:16:20 +0200

    Empty state#127 (#148)
    * Added a CertificateEmpty file to display an empty state given no certificates
    
    * Add create course CTA to empty certificate page
    
    * Adds no certificates image
    
    ---------
    
    Co-authored-by: LucaPurcilly <lp@enablment.com>

--- app/src/pages/Certificates.tsx
+++ app/src/pages/Certificates.tsx
@@ -6,7 +6,7 @@ export default function Certificates() {
   return (
     <Layout meta="Certificates">
       <div className='h-[93%] align-self-center lg:px-20 xl:px-40'>
-        <div className='m-8 p-8 pb-0 bg-white rounded-xl overflow-hidden min-h-full'>
+        <div className='m-8 p-8 pb-0 bg-white rounded-xl overflow-hidden h-full'>
           <CertificateList />
         </div>
       </div>
