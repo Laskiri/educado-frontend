d51b8b3f90f9eb7b5a2672c30f8201464a70a3ac
Martin <maje22@student.aau.dk>
2024-10-11 14:55:08 +0200

    Experimental changes to make /application layout align with /profile (very WIP!)
    Co-Authored-By: sigurdavh <sigurdavh@hotmail.com>
    Co-Authored-By: JakobT97 <94533850+JakobT97@users.noreply.github.com>

--- app/src/pages/Application.tsx
+++ app/src/pages/Application.tsx
@@ -12,6 +12,14 @@ import Motivation from '../components/Application/Motivation';
 import AcademicExperience from '../components/Application/AcademicExperience';
 import WorkExperience from '../components/Application/ProfessionalExperience';
 import GenericModalComponent from '../components/GenericModalComponent';
+import {Icon} from "@mdi/react";
+import {mdiChevronDown, mdiChevronUp} from "@mdi/js";
+import AcademicExperienceForm from "../components/ProfileForms/AcademicExperience";
+
+import Profile from "../pages/Profile";
+import dynamicForms from "../utilities/dynamicForms";
+import {tempObjects} from "../helpers/formStates";
+import ProfessionalExperienceForm from "../components/ProfileForms/ProfessionalExperience";
 
 const Application = () => {
 
@@ -20,6 +28,11 @@ const Application = () => {
 
   // State for the confirmation modal visibility
   const [isModalVisible, setIsModalVisible] = useState(false);
+
+  const [toggleMenu2, setToggleMenu2] = useState(false);
+  const [toggleMenu3, setToggleMenu3] = useState(false);
+
+
 
   // Functions to open and close the confirmation modal
   const openModal = () => {
@@ -37,6 +50,32 @@ const Application = () => {
 
   //Function for deciding the different values in the form
   const { register, handleSubmit, formState: { errors } } = useForm<NewApplication>();
+
+  const {
+    dynamicInputsFilled,
+    userID,
+    educationErrorState,
+    experienceErrorState,
+    experienceErrors,
+    educationErrors,
+    handleExperienceInputChange,
+    handleCountExperience,
+    handleExperienceDelete,
+    addNewExperienceForm,
+    handleEducationDelete,
+    addNewEducationForm,
+    SubmitValidation,
+    submitError,
+    handleEducationInputChange,
+    experienceformData,
+    educationformData,
+    fetchDynamicData,
+    handleCheckboxChange,
+  } = dynamicForms();
+
+  const { emptyAcademicObject, emptyProfessionalObject } = tempObjects();
+
+
 
   /**
     * OnSubmit function for Application.
@@ -62,8 +101,11 @@ const Application = () => {
     });
   };
 
+
   return (
     <main className="flex flex-col min-h-screen bg-gradient-to-br from-[#C9E5EC] 0% to-[#FFF] 100%" >
+    {/*<main className="flex-grow overflow-x-hidden bg-[#E4F2F5] h-screen">*/}
+
 
       { /* Navbar */ }
       <nav className="flex fixed w-full items-center justify-between bg-secondary box-shadow-md bg-fixed top-0 left-0 right-0 z-50 bg-F1F9FB shadow-[0px 4px 4px 0px rgba(35, 100, 130, 0.25)]">
@@ -76,43 +118,171 @@ const Application = () => {
         </div>
       </nav>
 
-      <body className="relative right-0 h-full flex-1 flex flex-col z-10">
-        <form className="relative right-0 w-full overflow-none flex-1 h-screen flex flex-col items-center gap-5 z-50" onSubmit={handleSubmit(onSubmit)}>
+      <div className="relative right-0 h-full flex-1 flex flex-col z-10">
 
-          {/* Box for text */}
-          <div className="items-center p-10 pt-20">
-            <h1 className="text-center text-cyan-800 text-[32px] font-bold font-['Montserrat']">
-              Que bom que você quer fazer parte do Educado! {/* Glad you want to be part of Educado! */}
-            </h1>
-            <p className="text-center text-neutral-700 text-lg font-normal font-['Montserrat']">
-              {/* We need some information to approve your content creator access. We'll get back to you with an answer via e-mail. */}
-              Precisamos de algumas informações para aprovar seu acesso de criador de conteúdo. Retornaremos com uma resposta via e-mail
-            </p>
-          </div>
+        {/* Forms */}
+        {/*<form className="text-center py-8 px-10 w-full"*/}
+        <form className="relative right-0 w-full overflow-none flex-1 h-screen flex flex-col items-center gap-5 z-50"
+              onSubmit={handleSubmit(onSubmit)}
+        >
+          <div className="inline-block">
+            {/* Box for text */}
+            <div className="items-center p-10 pt-20">
+              <h1 className="text-center text-cyan-800 text-[32px] font-bold font-['Montserrat']">
+                Que bom que você quer fazer parte do Educado! {/* Glad you want to be part of Educado! */}
+              </h1>
+              <p className="text-center text-neutral-700 text-lg font-normal font-['Montserrat']">
+                {/* We need some information to approve your content creator access. We'll get back to you with an answer via e-mail. */}
+                Precisamos de algumas informações para aprovar seu acesso de criador de conteúdo. Retornaremos com uma
+                resposta via e-mail
+              </p>
+            </div>
 
-          <Motivation register={register} errors={errors} setIsMotivationFilled={setIsMotivationFilled} />
-          <AcademicExperience register={register} errors={errors} />
-          <WorkExperience register={register} errors={errors} />
+            <>
+              <Motivation register={register} errors={errors} setIsMotivationFilled={setIsMotivationFilled}/>
+              {/*<AcademicExperience register={register} errors={errors} />*/}
 
-          {/* Bottom div */}
-          <div className="w-[65%] flex justify-end">
+              {/* Menu for academic experience */}
 
-            {/* Send for analysis */}
-            <button type="button" onClick={openModal}
-              className={`h-[52px] px-10 py-4 rounded-lg justify-center items-center gap-2.5 flex text-center text-lg font-bold font-['Montserrat'] ${
-                // Opacity dimmed when button is disabled
-                isMotivationFilled 
-                    ? 'bg-primary hover:bg-cyan-900 text-white' 
-                    : 'bg-primary text-gray-200 cursor-not-allowed opacity-60'}`}
+              <div className="Menu-2-academic-experience inline-block">
+                <button
+                    type="button"
+                    className={`second_form_open w-[1000px] h-[72px] p-6 shadow-xl flex-col justify-start items-start gap-20 inline-flex font-bold font-montserrat pl-6 ${
+                        toggleMenu2
+                            ? "rounded-tl-lg rounded-tr-lg bg-[#166276] text-[#FFFFFF]"
+                            : "rounded-lg bg-white text-neutral-700 text-[#383838]"
+                    }`}
+                    onClick={() => setToggleMenu2(!toggleMenu2)}
+                >
+                  <div className="flex items-start">
+                    {toggleMenu2 ? (
+                        <Icon path={mdiChevronUp} size={1} color="#FFFFFF"/>
+                    ) : (
+                        <Icon path={mdiChevronDown} size={1} color="#383838"/>
+                    )}
+                    Experiências acadêmicas
+                  </div>
+                </button>
+              </div>
 
-              // Button is disabled if motivation form field is not filled out
-              disabled={!isMotivationFilled}
-            >
-              Enviar para análise
-            </button>
+              {/* Academic experience form */}
+              {/* Conditional rendering empty form when database is empty */}
+              {toggleMenu2 ? (
+                  educationformData.length === 0 ? (
+                      <AcademicExperienceForm
+                          key={0}
+                          index={0}
+                          educationformData={emptyAcademicObject}
+                          handleEducationInputChange={handleEducationInputChange}
+                          educationErrors={educationErrors}
+                          addNewEducationForm={addNewEducationForm}
+                          handleEducationDelete={handleEducationDelete}
+                      />
+                  ) : (
+                      educationformData.map((form, index) => (
+                          // Retrieve child compononent
+                          <AcademicExperienceForm
+                              key={index}
+                              index={index}
+                              educationformData={educationformData}
+                              handleEducationInputChange={handleEducationInputChange}
+                              educationErrors={educationErrors}
+                              addNewEducationForm={addNewEducationForm}
+                              handleEducationDelete={handleEducationDelete}
+                          />
+                      ))
+                  )
+              ) : (
+                  <></>
+              )}
+
+              {/*<div className="hidden sm:block" aria-hidden="true">
+                  <div className="py-3"></div>
+                </div>*/}
+
+
+              {/*<WorkExperience register={register} errors={errors} />*/}
+
+              {/* Menu for professional experience */}
+              <>
+                <div className="Menu-3-professional-experience ">
+                  <button
+                      type="button"
+                      className={`third_form_open w-[1000px] h-[72px] p-6 shadow-xl flex-col justify-start items-start gap-20 inline-flex font-bold font-montserrat pl-6 ${
+                          toggleMenu3
+                              ? "rounded-tl-lg rounded-tr-lg bg-[#166276] text-[#FFFFFF]"
+                              : "rounded-lg bg-white text-neutral-700 text-[#383838]"
+                      }`}
+                      onClick={() => setToggleMenu3(!toggleMenu3)}
+                  >
+                    <div className="flex items-start">
+                      {toggleMenu3 ? (
+                          <Icon path={mdiChevronUp} size={1} color="#FFFFFF"/>
+                      ) : (
+                          <Icon path={mdiChevronDown} size={1} color="#383838"/>
+                      )}
+                      Experiências profisisonais
+                    </div>
+                  </button>
+                </div>
+
+                {/* Professional experience form */}
+                {/* Conditional rendering empty form when database is empty */}
+                {toggleMenu3 ? (
+                    experienceformData.length === 0 ? (
+                        <ProfessionalExperienceForm
+                            key={0}
+                            index={0}
+                            experienceformData={emptyProfessionalObject}
+                            handleExperienceInputChange={handleExperienceInputChange}
+                            experienceErrors={experienceErrors}
+                            addNewExperienceForm={addNewExperienceForm}
+                            handleExperienceDelete={handleExperienceDelete}
+                            handleCountExperience={handleCountExperience}
+                            handleCheckboxChange={handleCheckboxChange}
+                        />
+                    ) : (
+                        experienceformData.map((form, index) => (
+                            <ProfessionalExperienceForm
+                                key={index}
+                                index={index}
+                                experienceformData={experienceformData}
+                                handleExperienceInputChange={handleExperienceInputChange}
+                                experienceErrors={experienceErrors}
+                                addNewExperienceForm={addNewExperienceForm}
+                                handleExperienceDelete={handleExperienceDelete}
+                                handleCountExperience={handleCountExperience}
+                                handleCheckboxChange={handleCheckboxChange}
+                            />
+                        ))
+                    )
+                ) : (
+                    <></>
+                )}
+              </>
+
+
+              {/* Bottom div */}
+              <div className="w-[65%] flex justify-end">
+
+                {/* Send for analysis */}
+                <button type="button" onClick={openModal}
+                        className={`h-[52px] px-10 py-4 rounded-lg justify-center items-center gap-2.5 flex text-center text-lg font-bold font-['Montserrat'] ${
+                            // Opacity dimmed when button is disabled
+                            isMotivationFilled
+                                ? 'bg-primary hover:bg-cyan-900 text-white'
+                                : 'bg-primary text-gray-200 cursor-not-allowed opacity-60'}`}
+
+                    // Button is disabled if motivation form field is not filled out
+                        disabled={!isMotivationFilled}
+                >
+                  Enviar para análise
+                </button>
+              </div>
+            </>
           </div>
         </form>
-      </body>
+      </div>
 
       {/* Confirmation modal */}
       <GenericModalComponent
