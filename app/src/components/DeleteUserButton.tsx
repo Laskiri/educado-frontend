a45c62a561e70c0c1aef7797137bb31d1b567417
sourtendon <113062702+sourtendon@users.noreply.github.com>
2024-10-11 12:14:56 +0200

    Added functionality to admin toggle button Switched out delete user modal for generic modal component

--- app/src/components/DeleteUserButton.tsx
+++ app/src/components/DeleteUserButton.tsx
@@ -3,6 +3,7 @@ import { getUserToken } from '../helpers/userInfo';
 import AdminServices from '../services/admin.services';
 import { toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import GenericModalComponent from './GenericModalComponent';
 
 interface DeleteUserButtonProps {
   applicationId: string;
@@ -68,26 +69,10 @@ const DeleteUserButton: React.FC<DeleteUserButtonProps> = ({ applicationId, onDe
 
       {/* Confirmation Modal */}
       {showModal && (
-        <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50">
-          <div className="bg-white p-6 rounded shadow-lg">
-            <h3 className="text-lg font-bold mb-4">Are you sure you want to delete this user?</h3>
-            <div className="flex justify-end">
-              <button
-                onClick={handleCancel}
-                className="bg-gray-300 text-gray-800 px-4 py-2 rounded mr-2"
-              >
-                Cancel
-              </button>
-              <button
-                onClick={handleDelete}
-                className="bg-red-500 text-white px-4 py-2 rounded"
-              >
-                Delete
-              </button>
-            </div>
-          </div>
-        </div>
+        <GenericModalComponent onConfirm={handleDelete} onClose={handleCancel} isVisible={showModal} confirmBtnText='Deletar' title='Você tem certeza de que deseja excluir este usuário?' contentText=''/>
       )}
+
+      
     </>
   );
 };
