7af8dc844733ef3fb80c9d3fee39ebc3d6abc1d9
madsWesth <72575386+madsWesth@users.noreply.github.com>
2024-11-05 14:34:27 +0100

    add cover images for courses

--- app/src/components/Courses/CourseListCard.tsx
+++ app/src/components/Courses/CourseListCard.tsx
@@ -1,10 +1,10 @@
 // interfaces
-import { Course } from "../../interfaces/Course"
+import { Course } from "../../interfaces/Course";
 
 // Components
 import StarRating from "../general/StarRating";
-import { Icon } from '@mdi/react';
-import { mdiPencil, mdiAccount } from '@mdi/js';
+import { Icon } from "@mdi/react";
+import { mdiPencil, mdiAccount } from "@mdi/js";
 import { LastEdited } from "./LastEdited";
 
 // Helpers
@@ -12,84 +12,138 @@ import categories from "../../helpers/courseCategories";
 import statuses from "../../helpers/courseStatuses";
 
 // Images
-import imageNotFoundImage from '../../assets/image-not-found.png'
+import imageNotFoundImage from "../../assets/image-not-found.png";
+import StorageServices from "../../services/storage.services";
+import { getUserToken } from "../../helpers/userInfo";
+import { useEffect, useState } from "react";
+import Loading from "../general/Loading";
 
 /**
  * Displays a course in a card format
- * 
+ *
  * @param {Course} course The course to be displayed
  * @returns HTML Element
  */
 export const CourseListCard = ({ course }: { course: Course }) => {
+  const [coverImage, setCoverImage] = useState(imageNotFoundImage);
+  const [isCoverImageLoading, setIsCoverImageLoading] = useState(true);
+
   const maxTitleLength = 20;
+
+  const userToken = getUserToken();
+
+  useEffect(() => {
+    const getImageBinary = async () => {
+      let image: string;
+      try {
+        if (course.coverImg !== undefined && course.coverImg !== "") {
+          const imgBinary = await StorageServices.getFile(
+            course.coverImg,
+            userToken
+          );
+          image = `data:image/jpeg;base64,${imgBinary}`;
+        } else {
+          image = imageNotFoundImage;
+        }
+
+        setIsCoverImageLoading(false);
+        setCoverImage(image);
+      } catch (err) {
+        console.error(err);
+        image = imageNotFoundImage;
+
+        setIsCoverImageLoading(false);
+        setCoverImage(image);
+      }
+    };
+
+    getImageBinary();
+  }, []);
+
   return (
     <div className="overflow-hidden shadow rounded h-full w-full cursor-pointer m-auto hover:shadow-lg duration-200">
       <label
-        onClick={()=>window.location.href = `/courses/manager/${course._id}/0`}
+        onClick={() =>
+          (window.location.href = `/courses/manager/${course._id}/0`)
+        }
         className="w-full block h-full relative group"
       >
-
         {/* Hover overlay */}
-        <div className='absolute inset-0 grid place-content-center'>
+        <div className="absolute inset-0 grid place-content-center">
           {/* Edit button */}
-          <div className='z-10 bg-primary opacity-0 rounded-full p-8 group-hover:animate-slidePopIn duration-200 group-hover:opacity-100'>
-            <Icon path={mdiPencil} className='w-12 text-white' />
+          <div className="z-10 bg-primary opacity-0 rounded-full p-8 group-hover:animate-slidePopIn duration-200 group-hover:opacity-100">
+            <Icon path={mdiPencil} className="w-12 text-white" />
           </div>
-          <div className='z-0 absolute inset-0 bg-white opacity-0 group-hover:opacity-30 duration-200' />
+          <div className="z-0 absolute inset-0 bg-white opacity-0 group-hover:opacity-30 duration-200" />
         </div>
 
         {/* Card image */}
-        <img
-          src={course.coverImg ?? imageNotFoundImage}
-          alt="Course cover image"
-          onError={({ currentTarget }) => {
-            currentTarget.onerror = null; // prevents looping
-            currentTarget.src = imageNotFoundImage;
-          }}
-          className="h-40 w-full object-cover bg-white border-b"
-        />
+        {isCoverImageLoading ? (
+          <Loading />
+        ) : (
+          <img
+            src={coverImage}
+            alt="Course cover image"
+            onError={({ currentTarget }) => {
+              currentTarget.onerror = null; // prevents looping
+              currentTarget.src = imageNotFoundImage;
+            }}
+            className="h-40 w-full object-cover bg-white border-b"
+          />
+        )}
 
         {/* Card content */}
         <div className="bg-white w-full">
-          <div className='p-4'>
+          <div className="p-4">
             {/* Card title */}
             <h2 className="text-gray-800 text-xl font-medium mb-2">
-              {course.title.slice(0, maxTitleLength) + (course.title.length > maxTitleLength ? '...' : '')}
+              {course.title.slice(0, maxTitleLength) +
+                (course.title.length > maxTitleLength ? "..." : "")}
             </h2>
 
             <div className="flex flex-row justify-between">
               {/* Course rating */}
-              <div className='w-[8rem]'>
+              <div className="w-[8rem]">
                 <StarRating rating={course.rating ?? 0} />
               </div>
               {/* Subsriber count */}
-              <div className='flex flex-row'>
-                <Icon path={mdiAccount} className='w-5 mr-1 inline-block mr-1' />
-                <p className='text-grayMedium'>{course.numOfSubscriptions}</p>
+              <div className="flex flex-row">
+                <Icon
+                  path={mdiAccount}
+                  className="w-5 mr-1 inline-block mr-1"
+                />
+                <p className="text-grayMedium">{course.numOfSubscriptions}</p>
               </div>
             </div>
 
             {/* Card info */}
-            <div className='h-20 flex flex-col justify-between'>
+            <div className="h-20 flex flex-col justify-between">
               {/* Course category */}
-              <div className='flex flex-row'>
+              <div className="flex flex-row">
                 <Icon
-                  path={categories[course.category]?.icon ?? categories.default.icon}
-                  className='w-4 mr-1'
+                  path={
+                    categories[course.category]?.icon ?? categories.default.icon
+                  }
+                  className="w-4 mr-1"
                 />
                 <p>{categories[course.category]?.br ?? course.category}</p>
               </div>
 
-              <div className='flex flex-row justify-between'>
+              <div className="flex flex-row justify-between">
                 {/* Course last modified */}
                 <div>
                   <LastEdited course={course} />
                 </div>
 
                 {/* Course status */}
-                <div className='flex flex-row'>
-                  <div className={'w-3 h-3 mx-2 rounded-full m-auto '+(statuses[course.status].color ?? statuses.default.color)} />
-                  <p className='italic'>
+                <div className="flex flex-row">
+                  <div
+                    className={
+                      "w-3 h-3 mx-2 rounded-full m-auto " +
+                      (statuses[course.status].color ?? statuses.default.color)
+                    }
+                  />
+                  <p className="italic">
                     {statuses[course.status].br ?? statuses.default.br}
                   </p>
                 </div>
@@ -99,5 +153,5 @@ export const CourseListCard = ({ course }: { course: Course }) => {
         </div>
       </label>
     </div>
-  )
-}
+  );
+};
--- app/src/services/storage.services.ts
+++ app/src/services/storage.services.ts
@@ -1,57 +1,61 @@
 import axios from "axios";
 
-// Backend URL from .env file (automatically injected) 
+// Backend URL from .env file (automatically injected)
 import { BACKEND_URL } from "../helpers/environment";
-
 
 //import { Storage } from '@google-cloud/storage';
 
-
 export interface StorageInterface {
-    uploadFile: (bucketName: string, file: any, id: string) => void;
-    downloadFile: (bucketName: string, id: string, file: any) => void; 
+  uploadFile: (bucketName: string, file: any, id: string) => void;
+  downloadFile: (bucketName: string, id: string, file: any) => void;
 }
 
 // Props interface for uploadFile function
 type FileProps = {
-    file: any,
-    id: string | undefined,
-    parentType: string
-}
+  file: any;
+  id: string | undefined;
+  parentType: string;
+};
 /**
  * Uploads a file to a bucket
- * @param {string} file - The local path to the file to upload 
+ * @param {string} file - The local path to the file to upload
  * @param {string} id - The id the file will be saved as in the bucket. Format: courseId/sectionsId/componentId/index or courseId/index
  * @param {string} pranetType - A single letter that represents the type of the parent component. Format: c for course, and  l for lecture
  * @returns {void}
-*/
-async function uploadFile({id, file, parentType: parentType}: FileProps) {
-    if (!file || !id) {
-        return;
-    }
-    axios.postForm(`${BACKEND_URL}/api/bucket/upload`, {
-        fileName: id + "_"+ parentType,
-        file: file
-    })
+ */
+async function uploadFile({ id, file, parentType: parentType }: FileProps) {
+  if (!file || !id) {
+    return;
+  }
+  axios.postForm(`${BACKEND_URL}/api/bucket/upload`, {
+    fileName: id + "_" + parentType,
+    file: file,
+  });
 }
 
-const getFile = async (url: string, token: string) => {
-    return await axios.get(url, { headers: { Authorization: `Bearer ${token}` } })
-      .then((res) => res.data);
-  };
+const getFile = async (imgId: string, token: string) => {
+  return await axios
+    .get(`${BACKEND_URL}/api/bucket/${imgId}`, {
+      headers: { Authorization: `Bearer ${token}` },
+    })
+    .then((res) => res.data);
+};
 
 const deleteFile = async (id: string | undefined, token: string) => {
-    if (!id) {
-        return;
-    }
-return await axios.delete(`${BACKEND_URL}/api/bucket/${id}`, { headers: { Authorization: `Bearer ${token}` } })
+  if (!id) {
+    return;
+  }
+  return await axios
+    .delete(`${BACKEND_URL}/api/bucket/${id}`, {
+      headers: { Authorization: `Bearer ${token}` },
+    })
     .then((res) => res.data);
 };
 
 const StorageServices = Object.freeze({
-    uploadFile,
-    getFile,
-    deleteFile
+  uploadFile,
+  getFile,
+  deleteFile,
 });
 
 export default StorageServices;
