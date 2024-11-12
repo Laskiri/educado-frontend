bfbeff947e28b096330ebf01b1df8587a51579e1
mathvangkilde <118834559+mathvangkilde@users.noreply.github.com>
2024-11-12 11:43:45 +0100

    state
    lidt kommentarer, loading. Emptystate anvendes statd

--- app/src/components/Certificates/CertificateList.tsx
+++ app/src/components/Certificates/CertificateList.tsx
@@ -7,41 +7,48 @@ import EmptyImg from "../../assets/no-courses.png";
 import EmptyState from "./CertificateEmpty";
 
 export default function CertificateList() {
+  const [certificates, setCertificates] = useState<Certificate[]>();
+  const [loading, setLoading] = useState(true); //loading istedet for emptyState? emptyState er fra CertificateEmpty
 
-	const [certificates, setCertificates] = useState<Certificate[]>();
+  useEffect(() => {
+    CertificateService.getUserCertificates(
+      localStorage.getItem("id") || ""
+    ).then((res: Certificate[]) => {
+      setCertificates(res);
+    });
+  }, []);
 
-	useEffect(() => {
-		CertificateService.getUserCertificates(localStorage.getItem('id') || '').then((res : Certificate[]) => {
-			setCertificates(res);
-		})
-	}, []);
+  if (!certificates) return <EmptyState />;
 
-	if (!certificates) return <EmptyState />;
-
-	return (
-		<div className="overflow-scroll min-h-full pb-4" id="certificate-list">
-			{(certificates.length > 0) ?
-				<>
-					<div className="w-full">
-						<h1 className="text-3xl font-semibold">Certificados</h1>
-						<p className="text-grayMedium">Você tem {certificates.length} certificados.</p>
-					</div>
-					{certificates.map((certificate: Certificate, key: number) => (
-						<CertificateCard certificate={certificate} key={key} num={key} />
-					))}
-				</> :
-				// If the user has no courses, display this 'empty state'
-				<div className='grid place-content-center w-full h-full text-center'>
-				<div className='md:mx-40 xl:mx-64' id="no-certificates-message">
-					<img src={EmptyImg} className='w-full' />
-					<h1 className='te	xt-xl font-bold my-4'>Comece agora</h1>
-					{/* It appears you haven't created any courses yet.
+  return (
+    <div className="overflow-scroll min-h-full pb-4" id="certificate-list">
+      {certificates.length > 0 ? ( //hvordan connecter den til certifikaterne=
+        <>
+          <div className="w-full">
+            <h1 className="text-3xl font-semibold">Certificados</h1>
+            <p className="text-grayMedium">
+              Você tem {certificates.length} certificados.
+            </p>
+          </div>
+          {certificates.map((certificate: Certificate, key: number) => (
+            <CertificateCard certificate={certificate} key={key} num={key} />
+          ))}
+        </>
+      ) : (
+        // If the user has no courses, display this 'empty state'
+        <div className="grid place-content-center w-full h-full text-center">
+          <div className="md:mx-40 xl:mx-64" id="no-certificates-message">
+            <img src={EmptyImg} className="w-full" />
+            <h1 className="te	xt-xl font-bold my-4">Comece agora</h1>
+            {/* It appears you haven't created any courses yet.
 					Click the button below to see your course page. */}
-					<p>Parece que você ainda não criou nenhum curso. Clique no botão abaixo para acessar sua página de cursos.</p>
-				</div>
-			</div>
-
-			}
-		</div>
-	)
+            <p>
+              Parece que você ainda não criou nenhum curso. Clique no botão
+              abaixo para acessar sua página de cursos.
+            </p>
+          </div>
+        </div>
+      )}
+    </div>
+  );
 }
--- app/src/pages/Certificates.tsx
+++ app/src/pages/Certificates.tsx
@@ -1,15 +1,14 @@
 import CertificateList from "../components/Certificates/CertificateList";
 import Layout from "../components/Layout";
 
-
 export default function Certificates() {
   return (
     <Layout meta="Certificates">
-      <div className='h-[93%] align-self-center lg:px-20 xl:px-40'>
-        <div className='m-8 p-8 pb-0 bg-white rounded-xl overflow-hidden h-full'>
+      <div className="h-[93%] align-self-center lg:px-20 xl:px-40">
+        <div className="m-8 p-8 pb-0 bg-white rounded-xl overflow-hidden h-full">
           <CertificateList />
         </div>
       </div>
     </Layout>
-  )
+  );
 }
