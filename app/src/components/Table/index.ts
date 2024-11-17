e224362ca3331daf760b1b78b77a9e364e722cbf
madsWesth <72575386+madsWesth@users.noreply.github.com>
2024-11-17 23:10:51 +0100

    add table components

---
+++ app/src/components/Table/index.ts
@@ -0,0 +1,6 @@
+export { TableContainer } from "./TableContainer";
+export { Table } from "./Table";
+export { TableHead } from "./TableHead";
+export { TableBody } from "./TableBody";
+export { TableRow } from "./TableRow";
+export { TableCell } from "./TableCell";
