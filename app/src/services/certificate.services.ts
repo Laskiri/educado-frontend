ddc4217f05d9e8ccff40f1b8ca9a27013b27c4dd
Anders <andersraae01@gmail.com>
2024-11-25 15:06:57 +0100

    groupTree 114 WIP: Added preview

--- app/src/services/certificate.services.ts
+++ app/src/services/certificate.services.ts
@@ -35,6 +35,10 @@ const createCertificate = async (certificate: CertificateIds) => {
 	);
 }
 
+const getCertificatePreview = async () => {
+	return await client.get('/api/creator-certificates/preview');
+}
+
 const getUserCertificates = async (id : string) => {
 	const certificates = await client.get('/api/creator-certificates/creator/' + id, {
 		headers: {
@@ -67,6 +71,7 @@ const CertificateService = Object.freeze({
 	createCertificate,
 	getUserCertificates,
 	deleteCertificate,
+	getCertificatePreview,
 });
 
 export default CertificateService;
